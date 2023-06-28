181133ea21cc393dae588c0a4f68afd05353aaca
JM <noteasymin@gmail.com>
2023-06-28 17:42:00 +0900

    create: crud

---
+++ src/main/java/com/jimin/book/springboot/service/posts/PostsService.java
@@ -0,0 +1,18 @@
+package com.jimin.book.springboot.service.posts;
+
+import com.jimin.book.springboot.domain.posts.PostsRepository;
+import com.jimin.book.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
